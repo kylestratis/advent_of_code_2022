9ec3c170949dac0fb5438f3bfd88b0046bf438bc
Kyle Stratis <kyle@vizit.com>
2022-12-08 22:19:27 -0500

    Adds solutions for Day 2 Part 2

--- day_2/day2_pt2.py
+++ day_2/day2_pt2.py
@@ -4,31 +4,27 @@ B X
 C Z
 """
 
-MODIFIER = {
-    "X": 1,
-    "Y": 2,
-    "Z": 3,
-}
 
-STRATEGY = {
-    "X": {"A": 3, "B": 0, "C": 6},
-    "Y": {"A": 6, "B": 3, "C": 0},
-    "Z": {"A": 0, "B": 6, "C": 3},
-}
+MODIFIER = {"A": 1, "B": 2, "C": 3}
+
+TRANSFORMER = {"X": (-1, 0), "Y": (0, 3), "Z": (1, 6)}
 
 
 def score_rounds(input_string: str) -> int:
     rounds = [line.split() for line in input_string.strip().split("\n")]
     score = 0
     for rnd in rounds:
-        score += STRATEGY[rnd[1]][rnd[0]] + MODIFIER[rnd[1]]
+        modifier = MODIFIER["ABC"[("ABC".index(rnd[0]) + TRANSFORMER[rnd[1]][0]) % 3]]
+        result = TRANSFORMER[rnd[1]][1]
+        score += modifier + result
     return score
 
 
 def score_rounds_unreadable(input_string: str) -> int:
     return sum(
         [
-            STRATEGY[rnd[1]][rnd[0]] + MODIFIER[rnd[1]]
+            MODIFIER["ABC"[("ABC".index(rnd[0]) + TRANSFORMER[rnd[1]][0]) % 3]]
+            + TRANSFORMER[rnd[1]][1]
             for rnd in [line.split() for line in input_string.strip().split("\n")]
         ]
     )
