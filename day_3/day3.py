48f9d1e104da00e302d82ab3efdb42d21a89ad8d
Kyle Stratis <kyle@vizit.com>
2022-12-12 23:03:21 -0500

    Adds day 3 part 2 solution

--- day_3/day3.py
+++ day_3/day3.py
@@ -13,6 +13,7 @@ SCORING = [chr(i) for i in range(97, 123)]
 SCORING.extend([chr(i) for i in range(65, 92)])
 
 
+# Part 1
 def compartmentalize(input_string: str) -> int:
     rucksacks = [line for line in input_string.strip().split("\n")]
     score = 0
@@ -27,8 +28,23 @@ def compartmentalize(input_string: str) -> int:
                 score += SCORING.index(item) + 1
     return score
 
+# Part 2
+def score_uniques(input_string: str) -> int:
+    rucksacks = [line for line in input_string.strip().split("\n")]
+    score = 0
+    group = []
+    for idx, rucksack in enumerate(rucksacks):
+        group.append(set(rucksack))
+        if (idx + 1) % 3 == 0:
+            unique = group[0].intersection(group[1]).intersection(group[2])
+            score += SCORING.index(unique.pop()) + 1
+            group.clear()
+    return score
+
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         input_string = f.read()
-        print(compartmentalize(input_string))
+        print(f"Part 1 score {compartmentalize(input_string)}")
+        print(score_uniques(TEST_INPUT))
+        print(f"Part 2 score {score_uniques(input_string)}")
